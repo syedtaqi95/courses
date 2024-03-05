8f0bd1bdf3e1ee0d8c0b9137c00c6afb151dc69e
syedtaqi95 <sh13226@my.bristol.ac.uk>
2024-03-05 23:42:19 +0000

    Fixed issues with running observer-pattern exercise

--- js-react-design-patterns/observer-pattern/index.js
+++ js-react-design-patterns/observer-pattern/index.js
@@ -1,23 +1,24 @@
-import './style.css';
 import {
   sendToGoogleAnalytics,
   sendToCustomAnalytics,
   sendToEmail,
-} from './analytics.js';
+} from "./analytics.js";
 
-const pinkBtn = document.getElementById('pink-btn');
-const blueBtn = document.getElementById('blue-btn');
+const pinkBtn = document.getElementById("pink-btn");
+const blueBtn = document.getElementById("blue-btn");
 
-pinkBtn.addEventListener('click', () => {
-  const data = '🎀 Click on pink button! 🎀';
+pinkBtn.addEventListener("click", () => {
+  const data = "🎀 Click on pink button! 🎀";
   sendToGoogleAnalytics(data);
   sendToCustomAnalytics(data);
   sendToEmail(data);
 });
 
-blueBtn.addEventListener('click', () => {
-  const data = '🦋 Click on blue button! 🦋';
+blueBtn.addEventListener("click", () => {
+  const data = "🦋 Click on blue button! 🦋";
   sendToGoogleAnalytics(data);
   sendToCustomAnalytics(data);
   sendToEmail(data);
 });
+
+sendToEmail();
