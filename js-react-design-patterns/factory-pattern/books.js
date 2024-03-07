8d53a4210d06de3030c2ca2c5275e0157bc92da9
syedtaqi95 <sh13226@my.bristol.ac.uk>
2024-03-07 22:51:21 +0000

    Completed factory pattern

--- js-react-design-patterns/factory-pattern/books.js
+++ js-react-design-patterns/factory-pattern/books.js
@@ -1,29 +1,23 @@
-export const book1 = {
-  title: 'Harry Potter',
-  author: 'JK Rowling',
-  isbn: 'AB123',
-};
+const createBook = (title, author, isbn) => ({
+  title,
+  author,
+  isbn,
+});
 
-export const book2 = {
-  title: 'The Great Gatsby',
-  author: 'F. Scott Fitzgerald',
-  isbn: 'CD456',
-};
+export const book1 = createBook("Harry Potter", "JK Rowling", "AB123");
 
-export const book3 = {
-  title: 'Moby-Dick',
-  author: 'Herman Melville',
-  isbn: 'EF789',
-};
+export const book2 = createBook(
+  "The Great Gatsby",
+  "F. Scott Fitzgerald",
+  "CD456",
+);
 
-export const book4 = {
-  title: 'Harry Potter',
-  author: 'JK Rowling',
-  isbn: 'AB123',
-};
+export const book3 = createBook("Moby-Dick", "Herman Melville", "EF789");
 
-export const book5 = {
-  title: 'The Great Gatsby',
-  author: 'F. Scott Fitzgerald',
-  isbn: 'CD456',
-};
+export const book4 = createBook("Harry Potter", "JK Rowling", "AB123");
+
+export const book5 = createBook(
+  "The Great Gatsby",
+  "F. Scott Fitzgerald",
+  "CD456",
+);
