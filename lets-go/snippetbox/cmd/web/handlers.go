39e0a8fdd245528c05c15d58c04cfdb54ce12988
syedtaqi95 <sh13226@my.bristol.ac.uk>
2024-04-10 20:00:38 +0100

    Added basic templates for home, nav and main

--- lets-go/snippetbox/cmd/web/handlers.go
+++ lets-go/snippetbox/cmd/web/handlers.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hello from Snippetbox"))
+
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
@@ -22,10 +42,10 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("Display a form for creating a new snippet..."))
+	w.Write([]byte("Display a form for creating a new snippet..."))
 }
 
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-  w.WriteHeader(http.StatusCreated)
-  w.Write([]byte("Save a new snippet..."))
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Save a new snippet..."))
 }
