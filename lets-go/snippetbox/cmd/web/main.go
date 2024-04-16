633f5f98af4b8fe837b9fcda5f5a8edf41c48f3f
syedtaqi95 <sh13226@my.bristol.ac.uk>
2024-04-16 23:19:52 +0100

    Added command line flags and structured logging

--- lets-go/snippetbox/cmd/web/main.go
+++ lets-go/snippetbox/cmd/web/main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	addr := flag.String("addr", ":4000", "http network address")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
+	mux := http.NewServeMux()
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("Starting server on :4000")
+	logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	err := http.ListenAndServe(*addr, mux)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
